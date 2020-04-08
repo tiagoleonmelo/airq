17e14485efb050bea7c39c3224eb6c8e8917a5ba
melo <tiagomelo@ua.pt>
2020-04-08 07:25:48 +0100

    Error page mapping

--- src/main/java/deti/tqs/airq/controller/CityNotFoundException.java
+++ src/main/java/deti/tqs/airq/controller/CityNotFoundException.java
@@ -1,14 +1,40 @@
 package deti.tqs.airq.controller;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
-public class CityNotFoundException extends RuntimeException {
+@Controller
+public class CityNotFoundException implements ErrorController {
 
-    /**
-     *
-     */
-    private static final long serialVersionUID = 1L;
+    static final Logger logger = Logger.getLogger(CityNotFoundException.class);
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            Integer statusCode = Integer.valueOf(status.toString());
+
+            if (statusCode == HttpStatus.NOT_FOUND.value()) {
+                logger.error("404 Page Not Found");
+            } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                logger.error("500 Internal Server error: check Spring logs");
+            }
+        }
+
+        return "error";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
 
 }
